816beefcf788679a37eba0f28984ac872f6ea256
CAB230 <cab230@VDI-VL24B-118.qut.edu.au>
2020-06-06 17:11:12 +1000

    Knex db added - removed old hacky db connection

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const db = require("./database/db"); // Old hacky way of dealiying with db
+// const db = require("./database/db"); // Old hacky way of dealiying with db
+const knex_options = require("./knexfile");
+const knex = require("knex")(knex_options);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -19,8 +21,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(db);
 
+// ------------- DATABASE ---------
+// app.use(db); // old hacky db code
+
+// Knex middleware to make db available for all routes
+app.use((req, res, next) => {
+  req.db = knex; // attach the knex db obj
+  next();
+});
+
+// Knex Test code
+// app.use("/knex", (req, res, next) => {
+//   req.db
+//     .raw("SELECT VERSION()")
+//     .then((version) => console.log(version[0][0]))
+//     .catch((err) => console.log(err));
+
+//   res.send("It worked");
+// });
+
+// ----------- Routres ------------
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -14,45 +14,82 @@ router.get("/api", function (req, res, next) {
 
 /* All Cities Page */
 router.get("/api/city", function (req, res) {
-  var query = "SELECT name, district FROM ??";
-  var table = ["city"];
-  query = mysql.format(query, table);
-  req.db.query(query, function (err, rows) {
-    if (err) {
-      res.json({
-        Error: true,
-        Message: "Error executing MySQL query",
-      });
-    } else {
+  // ---- old hacky db code ----
+  // var query = "SELECT name, district FROM ??";
+  // var table = ["city"];
+  // query = mysql.format(query, table);
+  // req.db.query(query, function (err, rows) {
+  //   if (err) {
+  //     res.json({
+  //       Error: true,
+  //       Message: "Error executing MySQL query",
+  //     });
+  //   } else {
+  //     res.json({
+  //       Error: false,
+  //       Message: "Success",
+  //       City: rows,
+  //     });
+  //   }
+  // });
+
+  req.db
+    .from("city")
+    .select("name", "district")
+    .then((rows) => {
       res.json({
         Error: false,
         Message: "Success",
         City: rows,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      res.json({
+        Error: true,
+        Message: "Error executing MySQL query",
+      });
+    });
 });
 
 /* Filtered Cities by Country */
 router.get("/api/city/:CountryCode", function (req, res) {
-  var query = "SELECT * FROM ?? WHERE ??=?";
-  var table = ["city", "CountryCode", req.params.CountryCode];
-  query = mysql.format(query, table);
+  // -------- Old hacky db code -------
+  // var query = "SELECT * FROM ?? WHERE ??=?";
+  // var table = ["city", "CountryCode", req.params.CountryCode];
+  // query = mysql.format(query, table);
 
-  req.db.query(query, function (err, rows) {
-    if (err) {
-      res.json({
-        Error: true,
-        Message: "Error executing MySQL query",
-      });
-    } else {
+  // req.db.query(query, function (err, rows) {
+  //   if (err) {
+  //     res.json({
+  //       Error: true,
+  //       Message: "Error executing MySQL query",
+  //     });
+  //   } else {
+  //     res.json({
+  //       Error: false,
+  //       Message: "Success",
+  //       City: rows,
+  //     });
+  //   }
+  // });
+
+  req.db
+    .from("city")
+    .select("*")
+    .where("CountryCode", "=", req.params.CountryCode)
+    .then((rows) => {
       res.json({
         Error: false,
         Message: "Success",
         City: rows,
       });
-    }
-  });
+    })
+    .catch((err) => {
+      res.json({
+        Error: true,
+        Message: "Error executing MySQL query",
+      });
+    });
 });
 
 module.exports = router;
